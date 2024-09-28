50bdf2606a21e4dcd4d36c8383eb6a3d3fec56bb
ana-ruth <114896716+ana-ruth@users.noreply.github.com>
2024-09-28 18:34:56 -0400

    fixed repeated IDs

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+/*
 const form = document.querySelector("#club");
+
+async function sendData()
+{
+    const formData = new FormData(form);
+
+    try{
+        const response = await fetch("", {
+            method: "POST",
+
+            body: formData,
+        
+        });
+        console.log(await response.json());    
+    } 
+    catch(e){
+        console.error(e);
+    }
+}
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    sendData();
+});
+
+*/
