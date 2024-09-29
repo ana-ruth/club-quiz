4b4438a4b53510f8f6d2468b82789325d43bc963
ana-ruth <114896716+ana-ruth@users.noreply.github.com>
2024-09-28 20:13:29 -0400

    function in script.js to store form responses in variables

--- script.js
+++ script.js
@@ -1,27 +1,26 @@
-/*
-const form = document.querySelector("#club");
-
-async function sendData()
+function getFormValues()
 {
-    const formData = new FormData(form);
+    //form responses
+    var name = document.getElementById('Name').value;
+    var email = document.getElementById("Email").value;
+    var field = document.getElementById('field').value;
+    var level = document.getElementById('level').value;
+    var gender = document.querySelector('input[name="gender-specific"]:checked').value;
+    var extracurricular = document.getElementById('extracurricular').value;
+    var volunteering = document.querySelector('input[name="volunteering"]:checked').value;
+    var ethnicity = document.querySelector('input[name="ethnicity"]:checked').value;
 
-    try{
-        const response = await fetch("", {
-            method: "POST",
 
-            body: formData,
-        
-        });
-        console.log(await response.json());    
-    } 
-    catch(e){
-        console.error(e);
-    }
+    /* //to view them on the website//
+    document.getElementById('result').innerHTML = 
+    "Name: "+name+"<br>"+
+    "Email: " + email + "<br>" +
+    "Field: "+field+"<br>"+
+    "Level: "+level+"<br>"+
+    "Gender: " + gender + "<br>" +
+    "Extracurricular: "+ extracurricular+"<br>"+
+    "Volunteering: "+volunteering + "<br>" + 
+    "Ethnicity: "+ethnicity;
+    */
+
 }
-
-form.addEventListener("submit", (event) => {
-    event.preventDefault();
-    sendData();
-});
-
-*/
